fe0e65280e21a7de7f892b878637c736f0d76c39
MVC <michael.colianna@me.com>
2021-01-30 22:47:20 -0500

    Adds base entries

---
+++ database/migrations/2021_01_31_034607_create_entries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pay_period_id');
+            $table->foreignId('payee_id')->nullable();
+            $table->string('name')->nullable();
+            $table->integer('amount')->default(0);
+            $table->boolean('scheduled')->default(false);
+            $table->boolean('reconciled')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
