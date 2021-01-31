e66c0b0999ec380f89e177f0b78a6728aa8b9686
MVC <michael.colianna@me.com>
2021-01-30 22:45:04 -0500

    Adds base pay periods

---
+++ app/Http/Controllers/PayPeriodController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PayPeriod;
+use Illuminate\Http\Request;
+
+class PayPeriodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PayPeriod  $payPeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PayPeriod $payPeriod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PayPeriod  $payPeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PayPeriod $payPeriod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PayPeriod  $payPeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PayPeriod $payPeriod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PayPeriod  $payPeriod
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PayPeriod $payPeriod)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_01_31_034334_create_pay_periods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayPeriodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pay_periods', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->integer('start')->default(0);
+            $table->integer('current')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pay_periods');
+    }
+}
