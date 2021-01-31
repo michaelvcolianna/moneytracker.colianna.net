710a446edadaa18d9c7f9df048d7d883d3f3006b
MVC <michael.colianna@me.com>
2021-01-30 22:45:58 -0500

    Adds base payees

---
+++ database/migrations/2021_01_31_034514_create_payees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('amount')->nullable();
+            $table->boolean('schedule')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payees');
+    }
+}
