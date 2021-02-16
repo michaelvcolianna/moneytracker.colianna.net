0c4e222cf26e4bdfeeb6a6a72e831c8fe65c19c8
MVC <michael.colianna@me.com>
2021-02-16 15:57:54 -0500

    Adds PWA stuff from old budget app

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,14 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- PWA -->
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="Money">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/iphone-retina.png') }}">
+        <link rel="apple-touch-startup-image" href="{{ asset('img/launch-screen.png') }}">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Commissioner:wght@100;400;700&display=swap" rel="stylesheet">
