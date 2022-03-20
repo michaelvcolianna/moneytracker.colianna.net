39d3dcedc8b7d066367afc67071fe4895466fea3
Michael Colianna <mcolianna@cronin-co.com>
2022-03-20 13:24:29 -0400

    Updates the asset pipeline

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+    .js('resources/js/app.js', 'storage/assets/js')
+    .sass('resources/css/app.scss', 'storage/assets/css')
+    ;
+
+if(mix.inProduction())
+{
+mix .version();
+}
