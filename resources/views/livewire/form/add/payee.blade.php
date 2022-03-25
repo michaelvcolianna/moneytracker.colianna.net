38beef4159dbda6c9873bc75db99ad55227b4cf0
Michael Colianna <mcolianna@cronin-co.com>
2022-03-24 21:30:51 -0400

    Adds large screen styles for payees

--- resources/views/livewire/form/add/payee.blade.php
+++ resources/views/livewire/form/add/payee.blade.php
@@ -10,6 +10,7 @@
             <x-form.field.input id="new-start" label="Schedule Start Day" type="number" step="1" min="1" max="31" />
             <x-form.field.input id="new-end" label="Schedule End Day" type="number" step="1" min="1" max="31" />
             <x-form.field.options id="new-month" label="Schedule Months" type="months" :options="['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']" :areAnySelected="$areAnySelected" />
+            <x-form.field.boolean id="new-active" label="Active for autocomplete" />
 
             @include('partials.button.save', ['label' => 'Payee'])
         </div>
