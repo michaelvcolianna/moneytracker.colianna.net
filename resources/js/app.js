a499364a8f9288bc8aa62c5ac6514464ae9bf177
Michael V. Colianna <michael.colianna@me.com>
2023-02-07 12:29:06 -0500

    Adds key to expand add new panel

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,12 +10,20 @@ Alpine.start()
  */
 document.onkeyup = (event) => {
   if(typeof event.key === 'string') {
+    let adder = document.querySelector('div[class^="add-"]')
+    let add = document.querySelector('a[href="#add"]')
     let key = event.key.toLowerCase()
 
     event ??= window.event
 
+    // Escape key to collapse expandables
     if(key === 'escape' || key === 'esc') {
       Livewire.emit('escapeKeyPressed')
+    }
+
+    // // A key to expand add new item
+    if(key === 'a' && add && !adder.classList.contains('expanded')) {
+      add.dispatchEvent(new Event('click'))
     }
   }
 }
