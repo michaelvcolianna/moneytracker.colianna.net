30a217dd427507cf82248c1677184653850ad3ea
Michael V. Colianna <michael.colianna@me.com>
2023-02-07 20:18:07 +0000

    Adds pwa stuff and tweaks for mobile use - Updates package lock url - Adds public files for manifest - Adds meta/links to layout for pwa

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>MoneyTracker</title>
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
+        <meta name="theme-color" content="#faf9f6">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="Money">
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="{{ asset('iphone-retina.png') }}"
+        >
+        <link
+            rel="apple-touch-startup-image"
+            href="{{ asset('launch-screen.png') }}"
+        >
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="{{ config('app.fonts') }}" rel="stylesheet" />
