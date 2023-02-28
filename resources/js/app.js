0b66845ff89df41e4defdc4c7e0b1021d7934053
Michael V. Colianna <michael.colianna@me.com>
2023-02-28 00:07:08 +0000

    Adds keyboard navigation for entries/forecast/payees

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,9 @@ document.onkeyup = (event) => {
   if(typeof event.key === 'string') {
     let adder = document.querySelector('div[class^="add-"]')
     let add = document.querySelector('a[href="#add"]')
+    let entries = document.querySelector('a[href$="entries"]')
+    let forecast = document.querySelector('a[href$="forecast"]')
+    let payees = document.querySelector('a[href$="payees"]')
     let key = event.key.toLowerCase()
 
     event ??= window.event
@@ -21,7 +24,7 @@ document.onkeyup = (event) => {
       Livewire.emit('escapeKeyPressed')
     }
 
-    // // A key to expand add new item
+    // A key to expand add new item
     if(
       key === 'a'
       &&
@@ -32,6 +35,33 @@ document.onkeyup = (event) => {
       document.activeElement.tagName.toLowerCase() !== 'input'
     ) {
       add.dispatchEvent(new Event('click'))
+    }
+
+    // E key to go to entries page
+    if(
+      key === 'e'
+      &&
+      document.activeElement.tagName.toLowerCase() !== 'input'
+    ) {
+      window.location = entries.href
+    }
+
+    // F key to go to forecast page
+    if(
+      key === 'f'
+      &&
+      document.activeElement.tagName.toLowerCase() !== 'input'
+    ) {
+      window.location = forecast.href
+    }
+
+    // P key to go to payees page
+    if(
+      key === 'p'
+      &&
+      document.activeElement.tagName.toLowerCase() !== 'input'
+    ) {
+      window.location = payees.href
     }
   }
 }
