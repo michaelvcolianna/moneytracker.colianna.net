d84fabc13f737ba3b8785c02e0f5388d79df7cec
Michael V. Colianna <michael.colianna@me.com>
2023-10-20 11:27:28 -0400

    Adds routes and needed authentication portion

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Livewire\Entries;
+use App\Http\Livewire\Forecast;
+use App\Http\Livewire\Payees;
+use App\Models\Payday;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'payday'])->group(function() {
+    Route::get('/entries', Entries::class)->name('entries');
+
+    Route::get('/forecast', Forecast::class)->name('forecast');
+
+    Route::get('/payees', Payees::class)->name('payees');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+});
+
+Route::middleware('guest')->group(function() {
+    Route::redirect('/', '/login');
+
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 });
