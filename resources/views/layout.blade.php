54623adff10fe24b0bdad8f04c0c95366ac7ab82
Michael V. Colianna <michael.colianna@me.com>
2023-10-30 07:58:44 -0400

    Updates theme color in head

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>MoneyTracker</title>
         <link rel="manifest" href="{{ asset('manifest.json') }}">
-        <meta name="theme-color" content="#faf9f6">
+        <meta name="theme-color" content="#{{ session()->has('dark') ? '0d0805' : 'faf9f6' }}">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-title" content="Money">
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('iphone-retina.png') }}">
